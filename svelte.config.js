e2c5c9d2853d748e512158350fc74e001343c09d
mightymax <mark@lindeman.nu>
2025-03-26 09:08:49 +0100

    allow cross-site post

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,12 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+    // this is not safe, but what to do ...
+    // @see https://svelte.dev/docs/kit/configuration#csrf
+    csrf: {
+      checkOrigin: false
+    }
 	}
 };
 
